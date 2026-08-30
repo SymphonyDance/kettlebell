127fcdd22418503d2cbb93ded4707800753a0869
SymphonyDance <31779002+SymphonyDance@users.noreply.github.com>
2026-08-30 14:59:53 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'foundry-v5';
+const CACHE = 'foundry-v8';
 const CORE = ['./', 'index.html', 'sync.js', 'firebase-config.js', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png', 'icon-180.png'];
 
 self.addEventListener('install', e => {
